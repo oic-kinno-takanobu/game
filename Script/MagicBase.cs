48996e862ef10b801227de4c7316d5b8bf041366
Kinno <kinno1816@gmail.com>
2017-12-15 01:29:16 +0900

    エフェクト追加

---
+++ Script/MagicBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBase : MonoBehaviour
+{
+
+	protected float moveSpeed = 20f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Move ();
+	}
+
+	void Move ()
+	{
+		Vector3 vector = transform.forward * moveSpeed * Time.deltaTime;
+
+		transform.localPosition += vector;
+	}
+		
+}
